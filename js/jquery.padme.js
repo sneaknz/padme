69b0cde494042030b8dc807a384aa44dfc375079
Mike Harding <mike@sneak.co.nz>
2015-02-03 15:48:48 +1300

    Update examples, add allowExtras option

--- js/jquery.padme.js
+++ js/jquery.padme.js
@@ -1,6 +1,6 @@
 /* 
 	Padme plugin
-	v1.2
+	v1.0.3
 	Mike Harding
 	
 	A jQuery plugin to pad a justified list with placeholders, based on the number of columns.
@@ -16,14 +16,14 @@
 
 (function($) {
 
-	$.fn.padme = function(cols, options) {
+	$.fn.padme = function(options) {
 		
 		var opts = $.extend({}, $.fn.padme.defaults, options);
 		
 		function addPlaceholders(num, me) {
 			// Pad out the list with placeholders
 			var p = '';
-			for ( var i = 0; i < (opts.cols - num); i++ ) {
+			for ( var i = 0; i < (me.cols - num); i++ ) {
 				p += ' ' + me.placeholder;
 			}
 			me.$group.append(p);
@@ -41,76 +41,87 @@
 		}
 
 		return this.each(function() {
-			var me = {
+			var dataCols = $(this).data('padme-cols'),
+				me = {
 					$group: $(this),
-					cols: $(this).data('padme-cols')
+					cols: opts.cols ? opts.cols : dataCols
 				};
-			
-			opts.cols = me.cols ? me.cols : cols;
-			
-			// If children are defined then use them, otherwise default to all children
-			if ( opts.children ) {
-				me.$items = me.$group.children(opts.children).not('.' + opts.placeholderClass);
-			} else {
-				me.$items = me.$group.children().not('.' + opts.placeholderClass);
-			}	
-			
-			if ( me.$items.length > 0 ) {
-				var el;
 				
-				if ( opts.placeholderEl ) {
-					el = opts.placeholderEl;
+			// If cols have been defined, then go for it
+			if (me.cols !== null && me.cols > 1 ) {
+				
+				// If children are defined then use them, otherwise default to all children
+				if ( opts.children ) {
+					me.$items = me.$group.children(opts.children).not('.' + opts.placeholderClass);
 				} else {
-					el = me.$items.eq(0).prop('tagName');
-				}
+					me.$items = me.$group.children().not('.' + opts.placeholderClass);
+				}	
 				
-				me.placeholder = '<' + el + ' class="' + opts.placeholderClass + '"></' + el + '>';
+				if ( me.$items.length > 0 ) {
+					var el;
+				
+					if ( opts.placeholderEl ) {
+						el = opts.placeholderEl;
+					} else {
+						el = me.$items.eq(0).prop('tagName');
+					}
+				
+					me.placeholder = '<' + el + ' class="' + opts.placeholderClass + '"></' + el + '>';
 
-				me.$group.find('.' + opts.placeholderClass).remove();
-				
-				// Get top position of last item
-				var itemIndex = me.$items.length - 1;
-				me.top = me.$items.eq( itemIndex ).position().top;
-				var	count = 1,
-					currTop = me.top,
-					exit = false;
-				
-				// Check for previous siblings
-				while ( count < opts.cols && !exit && itemIndex > 0 ) {
-					itemIndex--;
-
-					var $prevItem = me.$items.eq( itemIndex );
+					me.$group.find('.' + opts.placeholderClass).remove();
 					
-					if ( $prevItem.length ) {
-						currTop = $prevItem.position().top;
+					var	count = 1;
+					
+					if ( opts.allowExtras ) {
+						// Get top position of last item
+						var itemIndex = me.$items.length - 1;
+						me.top = me.$items.eq( itemIndex ).position().top;
+						var	count = 1,
+							currTop = me.top,
+							exit = false;
+				
+						// Check for previous siblings
+						while ( count < me.cols && !exit && itemIndex > 0 ) {
+							itemIndex--;
 
-						if ( currTop >= me.top ) {
-							count++;
-						} else {
-							exit = true;
+							var $prevItem = me.$items.eq( itemIndex );
+					
+							if ( $prevItem.length ) {
+								currTop = $prevItem.position().top;
+
+								if ( currTop >= me.top ) {
+									count++;
+								} else {
+									exit = true;
+								}
+							} else {
+								exit = true;
+							}
 						}
 					} else {
-						exit = true;
+						count = me.$items.length % me.cols;
+					}
+
+					addPlaceholders(count, me);
+
+					if ( opts.removeOverflow ) {
+						removeOverflow(me);
 					}
 				}
 				
-				addPlaceholders(count, me);
-
-				if ( opts.removeOverflow ) {
-					removeOverflow(me);
-				}
 			}
-			
 		});
 		
 	};
 	
 	// plugin defaults
 	$.fn.padme.defaults = {
+		cols: null,
 		children: null,
 		placeholderEl: null,
 		placeholderClass: 'placeholder',
-		removeOverflow: false
+		removeOverflow: false,
+		allowExtras: false
 	};
 
 })(jQuery);
